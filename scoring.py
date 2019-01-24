cc6de833ce761e3ab3cf17f7b287c3f66211033c
UrbaNinja3393 <UrbaNinja3393@users.noreply.github.com>
2019-01-24 09:15:50 -0500

    Yeet
     On em

--- scoring.py
+++ scoring.py
@@ -11,20 +11,9 @@ Date: 01/03/2019
 Program Description: This uses the
 dataset to calculate a score.
 """
-<<<<<<< HEAD
 import sentiment
 
 #def Score(dataset1):		
 file = open('C:/Users/nogos/Documents/GitHub/Predicting-Elections/Test.txt', 'r+')
 sentiment.Sentiment(file)
 	#return 
-=======
-import random
-def Score(file, start, end):
-	#make flexible enough so we can use weights
-	"""
-	iterate through the file checking the date 
-	column and return the score.
-	"""
-	return random.randint(0,100), random.randint(0,100)
->>>>>>> 34ad915b3fadcbc924ffdd7ef59716be83ca999e
