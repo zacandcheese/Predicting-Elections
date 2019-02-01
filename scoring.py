a322ddaddd2b2a7afcfaffeec449485d90d429fd
zacandcheese <zacandcheese@icloud.com>
2019-01-31 21:57:24 -0500

    this is temporary

--- scoring.py
+++ scoring.py
@@ -14,6 +14,21 @@ dataset to calculate a score.
 import sentiment
 
 #def Score(dataset1):		
-file = open('C:/Users/nogos/Documents/GitHub/Predicting-Elections/Test.txt', 'r+')
-sentiment.Sentiment(file)
-	#return 
+#file = open('C:/Users/nogos/Documents/GitHub/Predicting-Elections/Test.txt', 'r+')
+#sentiment.Sentiment(file)
+#return 
+def Scoring(collection_of_tweets, end):
+	print("\nNEW LINE\n"+ end)
+	print(collection_of_tweets[end][0])
+			
+if __name__ == '__main__':
+	list_of_canidates = ["Wexton","Comstock"]
+	import json
+		
+	with open("tweets",'r') as fp:
+		collection_of_tweets = json.load(fp)
+	
+	coordinates_of_tweets = {}
+	for date in collection_of_tweets.keys():
+		for j in range(len(list_of_canidates)):
+			coordinates_of_tweets[date+chr(j+65)] = Scoring(collection_of_tweets, date)
